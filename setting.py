225e9d79ccf812c75ec9401636e345072bc9a975
Focus098xdd <focus.w.2553@gmail.com>
2025-02-18 12:31:04 +0700

    Update setting.py
    Change SMS APi

--- setting.py
+++ setting.py
@@ -1,6 +1,6 @@
 from colorama import Fore as c
 
 # cont varable
-URL = "https://app.snapp.taxi/api/api-passenger-oauth/v2/otp"
+URL = "https://api.1112delivery.com/api/v1/otp/create"
 INPUT_STRUCT = c.RED + " [" + c.WHITE + "~" + c.RED + "]"
 STRUCT = c.RED + " [" + c.WHITE + "!" + c.RED + "]"
